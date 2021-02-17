a488247b31ae1bd7f6c7b13208d5a7af98b5dbde
Kostya <79267873030@ya.ru>
2021-02-17 08:53:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+//console.log(1);
+//let number = 5;
+//const leftBorderWidth = 1;
+
+//number = 10;
+//console.log(number);
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(answer);
+//console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваша фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// document.write(answers);
+
+const category = "toys";
+console.log(`https://anyurl.com/${category}/5`);
+
+let names = prompt("Как ваше имя?", "Kostya");
+alert(`Hi, ${names}!`);
